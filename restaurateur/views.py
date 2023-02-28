32b0cc89d041530ccb90eb79bd092eaec5c32624
VneTraffiqua <safe72again@gmail.com>
2023-02-28 02:46:29 -0800

    List orders into manager UI

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
 
-
+from foodcartapp.models import Order
 from foodcartapp.models import Product, Restaurant
 
 
@@ -92,6 +92,7 @@ def view_restaurants(request):
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
+
     return render(request, template_name='order_items.html', context={
-        # TODO заглушка для нереализованного функционала
+        'order_items': Order.objects.all().order_by('-id')
     })
