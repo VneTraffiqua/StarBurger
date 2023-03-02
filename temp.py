607d2fae9e90fb7fe6c2b398a9e9636e99d6c1d2
VneTraffiqua <safe72again@gmail.com>
2023-03-02 00:24:43 -0800

    Delete temp.py

--- temp.py
+++
@@ -1,5 +0,0 @@
-s[0].products.all()[0].product.price * s[0].products.all()[0].quantity
-s = models.Order.objects.all()
-for item in s:
-    for product in item.products.all():
-        product.product.price * product.quantity
