0ae2db095212a2507a745a0b9450d72e2bf91834
VneTraffiqua <safe72again@gmail.com>
2023-03-17 01:29:39 -0700

    Update models.py

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -250,6 +250,7 @@ class OrderItem(models.Model):
         verbose_name='Цена',
         max_digits=6,
         decimal_places=2,
+        null=True,
         validators=[MinValueValidator(0)]
     )
 
