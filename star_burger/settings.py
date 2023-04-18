d2d955a772fce84cba43c1725ee50d641ad790c4
VneTraffiqua <safe72again@gmail.com>
2023-04-18 07:41:02 -0700

    Update settings.py

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -14,7 +14,7 @@ YANDEX_TOKEN = env('YANDEX_TOKEN')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost', 'starburger-vne.herokuapp.com'])
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
