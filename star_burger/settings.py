707ccab587ed5a9d0e16dc233e949d8ba4979798
VneTraffiqua <safe72again@gmail.com>
2023-04-25 07:00:18 -0700

    Update settings.py

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -92,11 +92,23 @@ WSGI_APPLICATION = 'star_burger.wsgi.application'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+#     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'starburger',
+        'USER': 'root',
+        'PASSWORD': env('DB_PASS'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
