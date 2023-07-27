b81fad094d1e74673db997b648a04f6d8f838075
VneTraffiqua <safe72again@gmail.com>
2023-07-27 00:04:49 -0700

    Update settings.py

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -2,7 +2,7 @@ import os
 import dj_database_url
 from environs import Env
 import psycopg2
-# Xyu`
+
 env = Env()
 env.read_env()
 
@@ -97,12 +97,6 @@ WSGI_APPLICATION = 'star_burger.wsgi.application'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-#     )
-# }
 
 DATABASES = {
     'default': {
