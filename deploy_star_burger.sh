340f344fcbe1b557725a2e5736a537205e73d822
VneTraffiqua <safe72again@gmail.com>
2023-07-27 12:53:00 -0700

    Create deploy_star_burger.sh

---
+++ deploy_star_burger.sh
@@ -0,0 +1,37 @@
+
+#!/bin/bash
+
+set -e
+
+
+cd /opt/StarBurger
+
+source .env
+
+git pull origin main
+
+source /opt/StarBurger/venv/bin/activate
+
+pip install -r requirements.txt
+
+python manage.py migrate --noinput
+
+npm ci --dev
+
+./node_modules/.bin/parcel watch bundles-src/index.js --dist-dir bundles --public-url="./"
+
+python manage.py collectstatic --noinput
+
+systemctl restart nginx
+
+hash=$(git rev-parse HEAD)
+
+curl https://api.rollbar.com/api/1/deploy/ \
+  -F access_token=$ROLLBAR_TOKEN \
+  -F environment=$ROLLBAR_ENVIROMENT \
+  -F revision=$hash \
+  -F local_username=$USER \
+  -F comment="Deployed new version" \
+  -F status=succeeded
+
+echo 'Deploy succes'
