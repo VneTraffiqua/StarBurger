cd7713e3f9b5ace5b8f00be7524ea693c56318d4
VneTraffiqua <safe72again@gmail.com>
2023-12-18 03:10:56 -0800

    Update settings.py

--- backend/star_burger/settings.py
+++ backend/star_burger/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = [
     '143.244.206.73', '64.226.99.16',
     '127.0.0.1', 'localhost', '127.0.0.0',
     'google.com', 'www.google-analytics.com',
+    'starburger', 
 ]
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
